24dcc909cb83a7aa14248c6edfdf39c621a33aba
basiccat0 <yunyaol@nycstudents.net>
2026-02-13 02:18:48 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -30,9 +32,16 @@ public class Main {
         }
 
         String[] hand = fileData.split("\n");
+
+        int[] type = new int[hand.length];
+        int[] bid = new int[hand.length];
+        String[][] allCards = new String[hand.length][5];
+
         for (int h = 0; h < hand.length; h++) {
-            String bid = hand[h].split("\\|")[0];
-            String[] deck = bid.split(",");
+            String cardPart = hand[h].split("\\|")[0];
+            int bidValue = Integer.parseInt(hand[h].split("\\|")[1]);
+            bid[h] = bidValue;
+            String[] deck = cardPart.split(",");
 
             for (int i = 0; i < deck.length; i++) {
                 deck[i] = deck[i].trim();
@@ -41,8 +50,9 @@ public class Main {
             boolean[] counted = new boolean[5];
 
             for (int i = 0; i < 5; i++) {
-                if (counted[i]) continue;
-
+                if (counted[i]) {
+                    continue;
+                }
                 int count = 1;
 
                 for (int j = i + 1; j < 5; j++) {
@@ -66,23 +76,32 @@ public class Main {
                     second = counts[i];
                 }
             }
-
+            int rank;
             if (max == 5) {
                 fiveKind++;
-
+                rank = 7;
             } else if (max == 4) {
                 fourKind++;
+                rank = 6;
             } else if (max == 3 && second == 2) {
                 fullHouse++;
+                rank = 5;
             } else if (max == 3) {
                 threeKind++;
+                rank = 4;
             } else if (max == 2 && second == 2) {
                 twoPair++;
+                rank = 3;
             } else if (max == 2) {
                 onePair++;
+                rank = 2;
             } else {
                 highCard++;
+                rank = 1;
             }
+
+            type[h] = rank;
+            allCards[h] = deck;
         }
 
         int total = 0;
@@ -95,8 +114,9 @@ public class Main {
                     rank++;
             }
 
-            total += (int) rank * bids[i];
+            total += (int) rank * bid[i];
         }
+
 
         System.out.println("Number of five of a kind hands: " + fiveKind);
         System.out.println("Number of full house hands: " + fullHouse);
@@ -105,5 +125,35 @@ public class Main {
         System.out.println("Number of two pair hands: " + twoPair);
         System.out.println("Number of one pair hands: " + onePair);
         System.out.println("Number of high card hands: " + highCard);
+        System.out.println("Total Bid Value: " + total);
+    }
+    public static int compare(int i, int j, String[][] cards, int[] type) {
+        if (type[i] != type[j]) {
+            return type[i] - type[j];
+        }
+        for (int k = 0; k < 5; k++) {
+            int a = convert(cards[i][k]);
+            int b = convert(cards[j][k]);
+
+            if (a != b) return a - b;
+        }
+
+        return 0;
+    }
+
+    public static int convert (String card) {
+        if (card.equals("Ace")) {
+            return 12;
+        }
+        if (card.equals("King")) {
+            return 11;
+        }
+        if (card.equals("Queen")) {
+            return 10;
+        }
+        if (card.equals("Jack")) {
+            return 9;
+        }
+        return Integer.parseInt(card) - 2;
     }
 }
